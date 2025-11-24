a4a06a205d471b39ec6303e2d7a4e12c046d515f
MartinGBB <martingbrazon@gmail.com>
2025-11-24 00:21:50 -0300

    feat: add docker ignore

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FaturamentoModule } from './modules/faturamento/faturamento.module';
 import { GestaoModule } from './modules/gestao/gestao.module';
+import { PlanosAtivosModule } from './modules/planosAtivos/planosAtivos.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     FaturamentoModule,
     GestaoModule,
+    PlanosAtivosModule,
   ],
 })
 export class AppModule {}
